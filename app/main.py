9a8869c84f6651639efcfbdba59d74ae9082da4b
ozysouza <ozysouzaa@gmail.com>
2024-10-05 19:16:40 -0400

    Create a Post class and store in a list for now

--- app/main.py
+++ app/main.py
@@ -5,6 +5,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "Title Post 1", "content": "Content of post 1", "id": 1},
+            {"title": "Favorite Food", "content": "I like pizza", "id": 2}]
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "Test"}
