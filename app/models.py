fb32c9fe1303e5f73030fed1c4c92cb659c1f2ac
ozysouza <ozysouzaa@gmail.com>
2024-11-09 19:34:28 -0500

    Add function to create user table on database

--- app/models.py
+++ app/models.py
@@ -9,7 +9,6 @@ from sqlmodel import Field, SQLModel
 
 class Post(SQLModel, table=True):
     __tablename__ = "posts"
-
     id: int = Field(primary_key=True, nullable=False)
     title: str = Field(nullable=False)
     content: str = Field(nullable=False)
@@ -19,3 +18,14 @@ class Post(SQLModel, table=True):
     created_at: Optional[datetime] = Field(
         sa_column=Column(TIMESTAMP(timezone=True), server_default=text('now()'))
     )
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    id: int = Field(primary_key=True, nullable=False)
+    email: str = Field(nullable=False, unique=True)
+    password: str = Field(nullable=False)
+    created_at: Optional[datetime] = Field(
+        sa_column=Column(TIMESTAMP(timezone=True), server_default=text('now()'))
+    )
+
