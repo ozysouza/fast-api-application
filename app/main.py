e3910d5200763389c213148c1bbfdda4eef5d9d7
ozysouza <ozysouzaa@gmail.com>
2024-11-18 12:48:36 -0500

    Add CORS to enable fetch from different origins

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import create_db_and_tables
 from app.routers import post, users, auth, vote
@@ -17,6 +18,16 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(users.router)
