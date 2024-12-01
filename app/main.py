42cb9d8b189c61d5c5fd9a4a2ad2035e2ace689b
ozysouza <ozysouzaa@gmail.com>
2024-11-30 22:57:44 -0500

    Add return message to easily identify change after deploy

--- app/main.py
+++ app/main.py
@@ -33,3 +33,8 @@ app.include_router(post.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World pushing out to AWS Instance"}
